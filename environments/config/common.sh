6fd74c1abb61bcd5ba54e689b8c466e92d93db5a
Tony Elmore <aelmore@pivotal.io>
2019-07-21 08:51:45 -0700

    start generate config for cf

--- environments/config/common.sh
+++ environments/config/common.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-export products=("cf" "p-isolation-segment" "p-healthwatch" "pivotal-mysql" "datadog" "apigee-edge-for-pcf-service-broker" "apmPostgres" "appdynamics" "p-dataflow" "p-rabbitmq" "p-redis" "p-scheduler" "p-spring-cloud-services" "Pivotal_Single_Sign-On_Service")
+export products=("cf" "pivotal-mysql" "p-dataflow" "p-rabbitmq" "p-redis" "apmPostgres" "Pivotal_Single_Sign-On_Service")
