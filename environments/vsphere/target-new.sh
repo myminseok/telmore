f61d406ef93a110bf5869e558d79a6ef771c59ad
Tony Elmore <aelmore@pivotal.io>
2020-02-20 14:59:39 -0700

    cleanup

--- environments/vsphere/target-new.sh
+++
@@ -1,7 +0,0 @@
-export OM_USERNAME=admin
-export OM_PASSWORD=bullswin
-export OM_TARGET=https://new-opsman.haas-244.pez.pivotal.io
-
-eval "$(om bosh-env)"
-
-export BOSH_ALL_PROXY="ssh+socks5://ubuntu@new-opsman.haas-244.pez.pivotal.io:22?private-key=/Users/anthonyelmore/.ssh/id_rsa"
