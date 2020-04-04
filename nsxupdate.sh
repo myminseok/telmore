ea1e0cf13237ec743b927ca58a02d0a6cb06ed1c
Tony Elmore <aelmore@pivotal.io>
2020-04-04 08:00:00 -0700

    move some files around

--- nsxupdate.sh
+++
@@ -1,1 +0,0 @@
-curl -k -v -u 'admin:oUB74nI8p5yuKcGRrS!' -X POST https://nsxmgr-01.haas-244.pez.pivotal.io/api/v1/loadbalancer/pools/f483cce7-b657-4a5e-a07f-2428e942f14a?action=ADD_MEMBERS --header "Content-Type: application/json" -d @nsxinput
